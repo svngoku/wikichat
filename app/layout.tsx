7002e904127e4d31dddfacf8a69a3f0f96cfaf6f
NIONGOLO Chrys Fé-Marty <chrysniongolo@gmail.com>
2023-08-07 08:19:26 +0200

    Update repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ChatHN – Chat with Hacker News using natural language",
+  title: "WikiChat – Chat with Wikipedia Articles using natural language",
   description:
-    "Chat with Hacker News using natural language. Built with OpenAI Functions and Vercel AI SDK.",
+    "Chat with Wikipedia Articles using natural language. Built with OpenAI Functions and Vercel AI SDK.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
